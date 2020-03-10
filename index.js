54a29ae504df0bd71b3f09bdecb8f34bea72d4b8
[Shinn Krammer] <[aa9e2d379a5e29cc15ac5aa7953e8ca0c32ac299]>
2020-03-10 19:45:14 +1300

    created file and code

---
+++ index.js
@@ -0,0 +1,8 @@
+//Task: Write a block of code that takes numbers and console log the sum of its cubes.
+let a = 5;
+let b = 3;
+let c = 9;
+
+result = a**3 + b**3 + c**3; // 125 + 27 + 729
+
+console.log(result) // = 881
